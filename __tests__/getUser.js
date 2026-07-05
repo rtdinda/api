5418fc8c6c8177f187bbe26e158bc74ddc5f8842
ratuadinda <rtdindaa@gmail.com>
2026-07-05 13:24:11 +0800

    2ndinit

--- __tests__/getUser.js
+++ __tests__/getUser.js
@@ -1,21 +1,22 @@
 import * as endpoint from '../endpoint/getUser';
 import * as schema from '../schema/getListUser';
-import * as respStatus from '../base/responseStatus';
+import respStatus from '../base/responseStatus';
 import * as er from '../base/expectResp';
 import {jest} from '@jest/globals';
 
 describe('Get List User -- Positive Case', () => {
   test('I can get list user and its detail by page number 2', async () => {
     const response = await endpoint.getListUsers(2);
-    console.log(response);
-    er.expectStatus(response, respStatus.statusSuccessful);
+    console.log(response.status, response.body, respStatus.statusSuccessful)
+    expect(respStatus).toEqual(respStatus.statusSuccessful);
+    er.expectStatus(response.status, respStatus.statusSuccessful);
     er.expectSchema(response, schema.listUsers);
     er.expectSnapshot(response);
   });
 });
 
 describe('Get User -- Positive Case', () => {
-  test('I can get detail user by ID 2', async () => {
+  test.skip('I can get detail user by ID 2', async () => {
     const response = await endpoint.getUser(2);
     console.log(response);
     er.expectStatus(response, respStatus.statusSuccessful);
--- endpoint/getUser.js
+++ endpoint/getUser.js
@@ -3,16 +3,16 @@ import supertest from 'supertest';
 
 import dotenv from 'dotenv';
 
-dotenv.config();
+require('dotenv').config({ path: '.env' });
 
 const api = supertest(process.env.BASE_URL);
 
 // export const getListUsers = (page) => api.get(`/api/users?page=${page}`)
-export const getListUsers = (page) => api.get(`/api/users?page=${page}`)
+export const getListUsers = (id) => api.get(`api/users?page=${id}`)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json');
 
 // export const getUser = (id) => api.get(`/api/users/${id}`)
-export const getUser = (id) => api.get(`/api/users/${id}`)
+export const getUser = (id) => api.get(`api/users/${id}`)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json');
